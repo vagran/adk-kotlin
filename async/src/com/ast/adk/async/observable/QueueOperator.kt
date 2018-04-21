cf20c5ec82816ce7326eb29818f323a7e89f35b6
Artyom <vagran.ast@gmail.com>
2018-04-21 22:44:11 +0300

    Implementing observable. Queue operator.

--- async/src/com/ast/adk/async/observable/QueueOperator.kt
+++ async/src/com/ast/adk/async/observable/QueueOperator.kt
@@ -11,26 +11,93 @@ import java.util.*
  */
 class QueueOperator<T>(input: Observable<T>,
                        private val maxSize: Int,
-                       private val tailDrop: Boolean):
-        ObservableOperator<T>() {
+                       private val tailDrop: Boolean): Observable.Source<T> {
+
+    val output: Observable<T> = Observable.Create(this)
 
     init {
         input.Subscribe(this::OnNext)
     }
 
-    private fun OnNext(value: Observable.Value<T>, error: Throwable?): Deferred<Boolean>?
+    override fun Get(): Deferred<Observable.Value<T>>
     {
-        val result = NextInput()
-        ProcessNext(value, error)
-        return result
+        var _lastValue: Observable.Value<T>? = null
+        var _lastError: Throwable? = null
+        var _valueProcessed: Deferred<Boolean>? = null
+        val _pendingResult: Deferred<Observable.Value<T>> = Deferred.Create()
+
+        synchronized(this) {
+            if (pendingResult != null) {
+                throw Error("Get() called while previous request not completed")
+            }
+            _lastValue = queue.pollFirst()
+            if (_lastValue == null) {
+                _lastError = pendingError
+            }
+
+            if (_lastValue != null || _lastError != null) {
+                _valueProcessed = valueProcessed
+                valueProcessed = null
+
+            } else {
+                pendingResult = _pendingResult
+            }
+        }
+
+        if (_lastError != null) {
+            _pendingResult.SetError(_lastError!!)
+        } else if (_lastValue != null) {
+            _pendingResult.SetResult(_lastValue!!)
+        }
+
+        if (_valueProcessed != null) {
+            _valueProcessed!!.SetResult(true)
+        }
+
+        return _pendingResult
     }
 
-    private fun ProcessNext(value: Observable.Value<T>, error: Throwable?)
+    private fun OnNext(value: Observable.Value<T>, error: Throwable?): Deferred<Boolean>?
     {
-        TODO()
+        var _valueProcessed: Deferred<Boolean>? = null
+        var _pendingResult: Deferred<Observable.Value<T>>? = null
+
+        synchronized(this) {
+            if (pendingResult != null) {
+                _pendingResult = pendingResult
+                pendingResult = null
+
+            } else {
+                if (error != null) {
+                    pendingError = error;
+                } else {
+                    if (tailDrop && queue.size >= maxSize) {
+                        queue.removeFirst()
+                    }
+                    queue.addLast(value)
+                    if (!tailDrop && queue.size >= maxSize) {
+                        valueProcessed = Deferred.Create()
+                        _valueProcessed = valueProcessed
+                    }
+                }
+            }
+        }
+
+        if (_pendingResult != null) {
+            if (error != null) {
+                _pendingResult!!.SetError(error)
+            } else {
+                _pendingResult!!.SetResult(value)
+            }
+        }
+
+        return _valueProcessed
     }
 
     private val queue: Deque<Observable.Value<T>> = ArrayDeque(maxSize)
+    private var pendingResult: Deferred<Observable.Value<T>>? = null
+    private var pendingError: Throwable? = null
+    private var valueProcessed: Deferred<Boolean>? = null
 }
 
 fun <T> Observable<T>.Queue(maxSize: Int, tailDrop: Boolean): Observable<T>
