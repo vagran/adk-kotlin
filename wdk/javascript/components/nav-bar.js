544f33de4c1584b5cb05794f81aa439660c32bb6
Artyom <vagran.ast@gmail.com>
2018-05-10 16:52:48 +0300

    Cosmetics.

--- wdk/javascript/components/nav-bar.js
+++ wdk/javascript/components/nav-bar.js
@@ -8,12 +8,12 @@ goog.provide("wdk.components.nav_bar");
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" :href="brandUrl">{{brand}}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" 
-          data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" 
+          data-target="#wdkNavbar" aria-controls="wdkNavbar" 
           aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <div class="collapse navbar-collapse" id="wdkNavbar">
         <ul class="navbar-nav mr-auto">
             <template v-for="item in items">
                 <li class="nav-item" :class="GetItemClasses(item)">
