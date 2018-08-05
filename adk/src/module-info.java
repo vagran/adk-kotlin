f7dce3098c2c2f6689aee229c0c4251d9395ed43
Artyom <vagran.ast@gmail.com>
2018-08-06 00:00:19 +0300

    Migrated to ArgParser.

--- adk/src/module-info.java
+++ adk/src/module-info.java
@@ -1,9 +1,9 @@
 module com.ast.adk {
     requires kotlin.stdlib;
     requires kotlin.stdlib.jdk8;
-
     requires log4j.api;
     requires log4j.core;
+    requires gson;
 
     exports com.ast.adk;
 }
