91770b1043b455f28525b0d315ee21d51d2fc3d0
Artyom <vagran.ast@gmail.com>
2018-09-10 18:09:34 +0300

    Implementing query parsing.

--- wdk/javascript/common/common.js
+++ wdk/javascript/common/common.js
@@ -4,3 +4,26 @@ goog.provide("wdk.common");
 
 goog.require("wdk.error");
 goog.require("wdk.class");
+
+(function(wdk) {
+
+    /** Parse URI query parameters into JS object.
+     * @param query Optional query string. Current location is used if not specified.
+     */
+    wdk.GetQueryParams = function (query) {
+        if (query === undefined) {
+            query = location.search;
+        }
+        if (query.length === 0) {
+            return {};
+        }
+        if (query[0] === '?') {
+            query = query.substring(1);
+        }
+        let pairStr = query.split('&');
+
+        console.log(pairStr);//XXX
+        return {aaa: 42};//XXX
+    };
+
+})(window.wdk || (window.wdk = {}));
