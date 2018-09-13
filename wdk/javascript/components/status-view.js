97487e5bbc773a033064cb56e6dd5d62f97b57f9
Artyom <vagran.ast@gmail.com>
2018-09-13 12:14:54 +0300

    Cosmetics.

--- wdk/javascript/components/status-view.js
+++ wdk/javascript/components/status-view.js
@@ -22,71 +22,24 @@ goog.provide("wdk.components.status_view");
     Vue.component("status-view", {
         template: tpl,
         props: ["status"],
+
         data() {
             return {
                 hidden: []
             }
         },
+
         watch: {
             status() {
                 this.hidden = [];
             }
         },
+
         computed: {
             items() {
                 if (this.status === null) {
                     return null;
                 }
-
-                let GetItem = function (obj) {
-                    let result = {details: null, inProgress: false};
-                    if ($.type(obj) === "string") {
-                        if (obj.startsWith("I>")) {
-                            result.alertClass = "alert-info";
-                            result.text = obj.substr(2);
-                        } else if (obj.startsWith("W>")) {
-                            result.alertClass = "alert-warning";
-                            result.text = obj.substr(2);
-                        } else if (obj.startsWith("E>")) {
-                            result.alertClass = "alert-danger";
-                            result.text = obj.substr(2);
-                        } else if (obj.startsWith("P>")) {
-                            result.alertClass = "alert-primary";
-                            result.text = obj.substr(2);
-                        } else if (obj.startsWith("S>")) {
-                            result.alertClass = "alert-success";
-                            result.text = obj.substr(2);
-                        } else if (obj.startsWith("%>")) {
-                            result.alertClass = "alert-secondary";
-                            result.text = obj.substr(2);
-                            result.inProgress = true;
-                        } else {
-                            result.alertClass = "alert-secondary";
-                            result.text = obj;
-                        }
-                        return result;
-
-                    } else if (obj.hasOwnProperty("responseJSON") && obj.responseJSON !== null &&
-                               obj.responseJSON.hasOwnProperty("message")) {
-
-                        result.alertClass = "alert-danger";
-                        result.text = obj.responseJSON.message;
-                        if (obj.responseJSON.hasOwnProperty("fullText")) {
-                            result.details = obj.responseJSON.fullText;
-                        }
-                        //XXX details
-                        return result;
-
-                    } else if (obj.hasOwnProperty("status")) {
-                        result.alertClass = "alert-danger";
-                        result.text = obj.status.toString();
-                        if (obj.hasOwnProperty("statusText")) {
-                            result.text += " " + obj.statusText;
-                        }
-                        return result;
-                    }
-                    return null;
-                };
 
                 let result = [];
                 if (Array.isArray(this.status)) {
@@ -95,13 +48,13 @@ goog.provide("wdk.components.status_view");
                             continue;
                         }
                         let item = this.status[index];
-                        let _item = GetItem(item);
+                        let _item = this._GetItem(item);
                         if (_item !== null) {
                             result.push(_item);
                         }
                     }
                 } else {
-                    let item = GetItem(this.status);
+                    let item = this._GetItem(this.status);
                     if (item !== null && !this.hidden.includes(0)) {
                         result.push(item);
                     }
@@ -124,8 +77,60 @@ goog.provide("wdk.components.status_view");
         },
 
         methods: {
-            OnDismiss(index) {
+            _OnDismiss(index) {
                 this.hidden.push(index);
+            },
+
+            _GetItem(obj) {
+                let result = {details: null, inProgress: false};
+                if ($.type(obj) === "string") {
+                    if (obj.startsWith("I>")) {
+                        result.alertClass = "alert-info";
+                        result.text = obj.substr(2);
+                    } else if (obj.startsWith("W>")) {
+                        result.alertClass = "alert-warning";
+                        result.text = obj.substr(2);
+                    } else if (obj.startsWith("E>")) {
+                        result.alertClass = "alert-danger";
+                        result.text = obj.substr(2);
+                    } else if (obj.startsWith("P>")) {
+                        result.alertClass = "alert-primary";
+                        result.text = obj.substr(2);
+                    } else if (obj.startsWith("S>")) {
+                        result.alertClass = "alert-success";
+                        result.text = obj.substr(2);
+                    } else if (obj.startsWith("%>")) {
+                        result.alertClass = "alert-secondary";
+                        result.text = obj.substr(2);
+                        result.inProgress = true;
+                    } else {
+                        result.alertClass = "alert-secondary";
+                        result.text = obj;
+                    }
+                    return result;
+
+                } else if (obj.hasOwnProperty("responseJSON") && obj.responseJSON !== null &&
+                    obj.responseJSON.hasOwnProperty("message")) {
+
+                    /* JSON error from backend. */
+                    result.alertClass = "alert-danger";
+                    result.text = obj.responseJSON.message;
+                    if (obj.responseJSON.hasOwnProperty("fullText")) {
+                        result.details = obj.responseJSON.fullText;
+                    }
+                    //XXX details
+                    return result;
+
+                } else if (obj.hasOwnProperty("status")) {
+                    /* jQuery XHR error. */
+                    result.alertClass = "alert-danger";
+                    result.text = obj.status.toString();
+                    if (obj.hasOwnProperty("statusText")) {
+                        result.text += " " + obj.statusText;
+                    }
+                    return result;
+                }
+                return null;
             }
         }
     });
