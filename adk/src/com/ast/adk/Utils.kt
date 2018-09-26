453af73d4c10bdb81ab2ae9d640c5485ccd098e8
Artyom <vagran.ast@gmail.com>
2018-09-26 17:41:24 +0300

    String.SplitByWhitespace()

--- adk/src/com/ast/adk/Utils.kt
+++ adk/src/com/ast/adk/Utils.kt
@@ -9,3 +9,27 @@ fun Throwable.GetStackTrace(): String
     printStackTrace(PrintWriter(sw))
     return sw.toString()
 }
+
+fun String.SplitByWhitespace(): List<String>
+{
+    var result: ArrayList<String>? = null
+
+    var lastStart = -1
+    for (i in 0 until length) {
+        val c = get(i)
+        if (Character.isWhitespace(c)) {
+            if (lastStart != -1) {
+                (result ?: ArrayList<String>().also {result = it}).add(substring(lastStart, i))
+                lastStart = -1
+            }
+        } else {
+            if (lastStart == -1) {
+                lastStart = i
+            }
+        }
+    }
+    if (lastStart != -1) {
+        return (result ?: ArrayList(1)).also {  it.add(substring(lastStart)) }
+    }
+    return result ?: emptyList()
+}
