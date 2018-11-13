53f2381f0f68e2c4da95d2d57e56e3bc3b4569ca
Artyom <vagran.ast@gmail.com>
2018-11-13 22:13:03 +0200

    Data class entity mutation helper.

--- domain/src/com/ast/adk/domain/Entity.kt
+++ domain/src/com/ast/adk/domain/Entity.kt
@@ -1,3 +1,21 @@
 package com.ast.adk.domain
 
-class Entity
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.reflect.KCallable
+import kotlin.reflect.KParameter
+
+/** Use it with data class copy() method.
+ * @param params Map with parameters to override.
+ */
+fun <T> MutateEntity(copyMethod: KCallable<T>, params: Map<String, Any?>): T
+{
+    val methodParams = copyMethod.parameters
+    val args = HashMap<KParameter, Any?>(methodParams.size)
+    for ((name, value) in params) {
+        val param = methodParams.find { it.name == name }
+            ?: throw Error("Unrecognized parameter: $name")
+        args[param] = value
+    }
+    return copyMethod.callBy(args)
+}
