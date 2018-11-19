fa72a39ca6edd44261284d9966efe4e240b64f22
Artyom <vagran.ast@gmail.com>
2018-11-19 14:28:03 +0200

    Move multiple deferred aggregation into extension.

---
+++ async/src/com/ast/adk/async/DeferredWhen.kt
@@ -0,0 +1,69 @@
+package com.ast.adk.async
+
+/** Create deferred which completes when all the specified deferred results are completed.
+ * If error occurs the first one is set to the resulted deferred.
+ */
+fun Deferred.Companion.When(results: Collection<Deferred<*>>): Deferred<Unit>
+{
+    return Deferred.When(results.iterator())
+}
+
+fun Deferred.Companion.When(vararg results: Deferred<*>): Deferred<Unit>
+{
+    return Deferred.When(results.iterator())
+}
+
+fun Deferred.Companion.When(results: Iterator<Deferred<*>>): Deferred<Unit>
+{
+    class Aggregator {
+
+        val result = Create<Unit>()
+        private var iterDone = false
+        private var numResults = 0
+        private var numComplete = 0
+        private var error: Throwable? = null
+
+        init {
+            while (results.hasNext()) {
+                val def = results.next();
+                synchronized(this) {
+                    numResults++
+                }
+                def.Subscribe(this::OnComplete)
+            }
+            synchronized(this) {
+                iterDone = true
+                CheckComplete()
+            }
+        }
+
+        @Suppress("UNUSED_PARAMETER")
+        private fun OnComplete(value: Any?, error: Throwable?)
+        {
+            synchronized(this) {
+                numComplete++
+                if (error != null) {
+                    if (this.error == null) {
+                        this.error = error
+                    } else {
+                        this.error!!.addSuppressed(error)
+                    }
+                }
+                CheckComplete()
+            }
+        }
+
+        private fun CheckComplete()
+        {
+            if (iterDone && numComplete == numResults) {
+                if (error == null) {
+                    result.SetResult(Unit)
+                } else {
+                    result.SetError(error as Throwable)
+                }
+            }
+        }
+    }
+
+    return Aggregator().result
+}
