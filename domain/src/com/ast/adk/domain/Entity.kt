2782eec2e07ac3c1aa91605f7ec862a5b72af413
Artyom <vagran.ast@gmail.com>
2018-12-05 10:30:58 +0200

    Automatic numeric types conversion for entities mutation.

--- domain/src/com/ast/adk/domain/Entity.kt
+++ domain/src/com/ast/adk/domain/Entity.kt
@@ -1,7 +1,10 @@
 package com.ast.adk.domain
 
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 import kotlin.reflect.KCallable
 import kotlin.reflect.KParameter
+import kotlin.reflect.jvm.jvmErasure
 
 /** Use it with data class instance copy() method.
  * @param params Map with parameters to override.
@@ -13,7 +16,37 @@ fun <T> MutateEntity(copyMethod: KCallable<T>, params: Map<String, Any?>): T
     for ((name, value) in params) {
         val param = methodParams.find { it.name == name }
             ?: throw Error("Unrecognized parameter: $name")
-        args[param] = value
+        var _value = value
+        if (value != null) {
+            val valCls = value::class
+            val paramCls = param.type.jvmErasure
+            if (valCls != paramCls) {
+                /* All numbers are represented as Double in JSON so convert them to other numeric
+                * type if necessary.
+                */
+                convert@do {
+                    if (value is Double) {
+                        when (paramCls) {
+                            Int::class -> {
+                                _value = value.roundToInt()
+                                break@convert
+                            }
+                            Long::class -> {
+                                _value = value.roundToLong()
+                                break@convert
+                            }
+                            Float::class -> {
+                                _value = value.toFloat()
+                                break@convert
+                            }
+                        }
+                    }
+                    throw Error(
+                        "Parameter ${param.name} type mismatch: have $valCls, expected $paramCls")
+                } while (false)
+            }
+        }
+        args[param] = _value
     }
     return copyMethod.callBy(args)
 }
