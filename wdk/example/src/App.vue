7125dd7704153842632d5ce91aa6914798a14178
Artyom <vagran.ast@gmail.com>
2020-09-10 10:26:03 +0300

    WDK example project.

---
+++ wdk/example/src/App.vue
@@ -0,0 +1,33 @@
+<!--
+  - This file is part of ADK project.
+  - Copyright (c) 2020 Artyom Lebedev <artyom.lebedev@gmail.com>. All rights reserved.
+  - See LICENSE file for full license details.
+  -->
+<template>
+    <div class="root">
+        WDK example application
+        <div class="number">{{i}}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            i: 42
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+
+.root {
+    color: brown;
+    .number {
+        color: darkblue;
+    }
+}
+
+</style>
---
+++ wdk/example/src/main.js
@@ -0,0 +1,13 @@
+/*
+ * This file is part of ADK project.
+ * Copyright (c) 2020 Artyom Lebedev <artyom.lebedev@gmail.com>. All rights reserved.
+ * See LICENSE file for full license details.
+ */
+
+import Vue from "vue";
+import App from "./App.vue";
+
+new Vue({
+    el: "#app",
+    render: h => h(App)
+});
