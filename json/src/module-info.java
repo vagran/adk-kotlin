c8ba439846f198a7945de7233bbdf1e86df37532
Artyom <vagran.ast@gmail.com>
2021-01-06 20:26:45 +0200

    JSON module has now transitive ADK base module dependency.

--- json/src/module-info.java
+++ json/src/module-info.java
@@ -2,7 +2,7 @@ module io.github.vagran.adk.json {
     requires kotlin.stdlib;
     requires kotlin.reflect;
     requires transitive io.github.vagran.adk.omm;
-    requires io.github.vagran.adk;
+    requires transitive io.github.vagran.adk;
 
     exports io.github.vagran.adk.json;
 }
